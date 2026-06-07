8fecb9cf36ef4cc8ef71c63ffc30108e6024e8dc
Shaun Phillips <smphillips@alumni.york.ac.uk>
2026-06-07 11:05:01 +0200

    Make everything is a bit neater

---
+++ decks/colours.js
@@ -0,0 +1,103 @@
+registerDeck("Colours", [
+
+{type:1,front:"红色",back:"Red"},
+{type:2,front:"Red",back:"红色\nhóng sè"},
+{type:3,front:"hong se",back:"Red\n\n红色\n\nhóng sè"},
+
+{type:1,front:"蓝色",back:"Blue"},
+{type:2,front:"Blue",back:"蓝色\nlán sè"},
+{type:3,front:"lan se",back:"Blue\n\n蓝色\n\nlán sè"},
+
+{type:1,front:"黄色",back:"Yellow"},
+{type:2,front:"Yellow",back:"黄色\nhuáng sè"},
+{type:3,front:"huang se",back:"Yellow\n\n黄色\n\nhuáng sè"},
+
+{type:1,front:"绿色",back:"Green"},
+{type:2,front:"Green",back:"绿色\nlǜ sè"},
+{type:3,front:"lü se",back:"Green\n\n绿色\n\nlǜ sè"},
+
+{type:1,front:"黑色",back:"Black"},
+{type:2,front:"Black",back:"黑色\nhēi sè"},
+{type:3,front:"hei se",back:"Black\n\n黑色\n\nhēi sè"},
+
+{type:1,front:"白色",back:"White"},
+{type:2,front:"White",back:"白色\nbái sè"},
+{type:3,front:"bai se",back:"White\n\n白色\n\nbái sè"},
+
+{type:1,front:"橙色",back:"Orange"},
+{type:2,front:"Orange",back:"橙色\nchéng sè"},
+{type:3,front:"cheng se",back:"Orange\n\n橙色\n\nchéng sè"},
+
+{type:1,front:"紫色",back:"Purple"},
+{type:2,front:"Purple",back:"紫色\nzǐ sè"},
+{type:3,front:"zi se",back:"Purple\n\n紫色\n\nzǐ sè"},
+
+{type:1,front:"粉红色",back:"Pink"},
+{type:2,front:"Pink",back:"粉红色\nfěn hóng sè"},
+{type:3,front:"fen hong se",back:"Pink\n\n粉红色\n\nfěn hóng sè"},
+
+{type:1,front:"棕色",back:"Brown"},
+{type:2,front:"Brown",back:"棕色\nzōng sè"},
+{type:3,front:"zong se",back:"Brown\n\n棕色\n\nzōng sè"},
+
+{
+type:4,
+front:"蓝色是我最喜欢的____。",
+back:"colour\n\n颜色\n\nyán sè\n\nOptions:\n1. 颜色\n2. 老师\n3. 医生\n4. 学生\n5. 名字"
+},
+
+{
+type:4,
+front:"我的工作是____。",
+back:"teacher\n\n老师\n\nlǎo shī\n\nOptions:\n1. 红色\n2. 老师\n3. 蓝色\n4. 黄色\n5. 颜色"
+},
+
+{
+type:4,
+front:"香蕉是____。",
+back:"yellow\n\n黄色\n\nhuáng sè\n\nOptions:\n1. 黄色\n2. 蓝色\n3. 紫色\n4. 棕色\n5. 黑色"
+},
+
+{
+type:4,
+front:"草是____。",
+back:"green\n\n绿色\n\nlǜ sè\n\nOptions:\n1. 白色\n2. 绿色\n3. 红色\n4. 棕色\n5. 黑色"
+},
+
+{
+type:4,
+front:"天空是____。",
+back:"blue\n\n蓝色\n\nlán sè\n\nOptions:\n1. 蓝色\n2. 黄色\n3. 棕色\n4. 紫色\n5. 粉红色"
+},
+
+{
+type:4,
+front:"熊猫是____和____。",
+back:"black and white\n\n黑色和白色\n\nhēi sè hé bái sè\n\nOptions:\n1. 黑色\n2. 白色\n3. 蓝色\n4. 红色\n5. 紫色"
+},
+
+{
+type:4,
+front:"巧克力通常是____。",
+back:"brown\n\n棕色\n\nzōng sè\n\nOptions:\n1. 棕色\n2. 绿色\n3. 蓝色\n4. 黄色\n5. 紫色"
+},
+
+{
+type:4,
+front:"葡萄可以是____。",
+back:"purple\n\n紫色\n\nzǐ sè\n\nOptions:\n1. 红色\n2. 蓝色\n3. 紫色\n4. 白色\n5. 黑色"
+},
+
+{
+type:4,
+front:"火车通常不是____。",
+back:"pink\n\n粉红色\n\nfěn hóng sè\n\nOptions:\n1. 粉红色\n2. 黑色\n3. 蓝色\n4. 红色\n5. 白色"
+},
+
+{
+type:4,
+front:"橙子是____。",
+back:"orange\n\n橙色\n\nchéng sè\n\nOptions:\n1. 蓝色\n2. 绿色\n3. 橙色\n4. 黑色\n5. 白色"
+}
+
+]);
