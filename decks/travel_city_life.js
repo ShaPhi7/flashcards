e17054de42e13a98d982824d14afe32cdeabfa1e
Shaun Phillips <smphillips@alumni.york.ac.uk>
2026-06-20 12:20:08 +0200

    Add travel deck

---
+++ decks/travel_city_life.js
@@ -0,0 +1,708 @@
+registerDeck("Travel & City Life", [
+
+{
+type:1,
+front:"一样",
+back:`一样\nyí yàng\nThe same / alike`
+},
+{
+type:2,
+front:"The same / alike",
+back:`一样\nyí yàng\nThe same / alike`
+},
+{
+type:3,
+front:"yi yang",
+back:`一样\nyí yàng\nThe same / alike`
+},
+
+{
+type:1,
+front:"哪儿",
+back:`哪儿\nnǎr\nWhere`
+},
+{
+type:2,
+front:"Where",
+back:`哪儿\nnǎr\nWhere`
+},
+{
+type:3,
+front:"nar",
+back:`哪儿\nnǎr\nWhere`
+},
+
+{
+type:1,
+front:"文化",
+back:`文化\nwén huà\nCulture`
+},
+{
+type:2,
+front:"Culture",
+back:`文化\nwén huà\nCulture`
+},
+{
+type:3,
+front:"wen hua",
+back:`文化\nwén huà\nCulture`
+},
+
+{
+type:1,
+front:"安全",
+back:`安全\nān quán\nSafe`
+},
+{
+type:2,
+front:"Safe",
+back:`安全\nān quán\nSafe`
+},
+{
+type:3,
+front:"an quan",
+back:`安全\nān quán\nSafe`
+},
+
+{
+type:1,
+front:"交通",
+back:`交通\njiāo tōng\nTraffic / transportation`
+},
+{
+type:2,
+front:"Traffic / transportation",
+back:`交通\njiāo tōng\nTraffic / transportation`
+},
+{
+type:3,
+front:"jiao tong",
+back:`交通\njiāo tōng\nTraffic / transportation`
+},
+
+{
+type:1,
+front:"方便",
+back:`方便\nfāng biàn\nConvenient`
+},
+{
+type:2,
+front:"Convenient",
+back:`方便\nfāng biàn\nConvenient`
+},
+{
+type:3,
+front:"fang bian",
+back:`方便\nfāng biàn\nConvenient`
+},
+
+{
+type:1,
+front:"堵车",
+back:`堵车\ndǔ chē\nTraffic jam`
+},
+{
+type:2,
+front:"Traffic jam",
+back:`堵车\ndǔ chē\nTraffic jam`
+},
+{
+type:3,
+front:"du che",
+back:`堵车\ndǔ chē\nTraffic jam`
+},
+
+{
+type:1,
+front:"商店",
+back:`商店\nshāng diàn\nShop / store`
+},
+{
+type:2,
+front:"Shop / store",
+back:`商店\nshāng diàn\nShop / store`
+},
+{
+type:3,
+front:"shang dian",
+back:`商店\nshāng diàn\nShop / store`
+},
+
+{
+type:1,
+front:"游客",
+back:`游客\nyóu kè\nTourist`
+},
+{
+type:2,
+front:"Tourist",
+back:`游客\nyóu kè\nTourist`
+},
+{
+type:3,
+front:"you ke",
+back:`游客\nyóu kè\nTourist`
+},
+
+{
+type:1,
+front:"热情",
+back:`热情\nrè qíng\nWarm / hospitable`
+},
+{
+type:2,
+front:"Warm / hospitable",
+back:`热情\nrè qíng\nWarm / hospitable`
+},
+{
+type:3,
+front:"re qing",
+back:`热情\nrè qíng\nWarm / hospitable`
+},
+
+{
+type:1,
+front:"城市",
+back:`城市\nchéng shì\nCity`
+},
+{
+type:2,
+front:"City",
+back:`城市\nchéng shì\nCity`
+},
+{
+type:3,
+front:"cheng shi",
+back:`城市\nchéng shì\nCity`
+},
+
+{
+type:1,
+front:"觉得",
+back:`觉得\njué de\nTo think / feel`
+},
+{
+type:2,
+front:"To think / feel",
+back:`觉得\njué de\nTo think / feel`
+},
+{
+type:3,
+front:"jue de",
+back:`觉得\njué de\nTo think / feel`
+},
+
+{
+type:1,
+front:"可以",
+back:`可以\nkě yǐ\nCan / may`
+},
+{
+type:2,
+front:"Can / may",
+back:`可以\nkě yǐ\nCan / may`
+},
+{
+type:3,
+front:"ke yi",
+back:`可以\nkě yǐ\nCan / may`
+},
+
+{
+type:1,
+front:"已经",
+back:`已经\nyǐ jīng\nAlready`
+},
+{
+type:2,
+front:"Already",
+back:`已经\nyǐ jīng\nAlready`
+},
+{
+type:3,
+front:"yi jing",
+back:`已经\nyǐ jīng\nAlready`
+},
+
+{
+type:1,
+front:"耐心",
+back:`耐心\nnài xīn\nPatient / patience`
+},
+{
+type:2,
+front:"Patient / patience",
+back:`耐心\nnài xīn\nPatient / patience`
+},
+{
+type:3,
+front:"nai xin",
+back:`耐心\nnài xīn\nPatient / patience`
+},
+
+{
+type:4,
+front:"这两个城市不____。",
+back:`These two cities aren't ____.
+
+这两个城市不____。
+zhè liǎng gè chéng shì bù ____.
+
+Options:
+
+1. 哪儿
+nǎr
+where
+
+2. 文化
+wén huà
+culture
+
+3. 一样
+yí yàng
+the same / alike
+
+4. 安全
+ān quán
+safe
+
+5. 交通
+jiāo tōng
+traffic`
+},
+
+{
+type:4,
+front:"你想去____玩？",
+back:`Where do you want to go to have fun?
+
+你想去____玩？
+nǐ xiǎng qù ____ wán?
+
+Options:
+
+1. 哪儿
+nǎr
+where
+
+2. 文化
+wén huà
+culture
+
+3. 安全
+ān quán
+safe
+
+4. 交通
+jiāo tōng
+traffic
+
+5. 方便
+fāng biàn
+convenient`
+},
+
+{
+type:4,
+front:"我很喜欢中国的____。",
+back:`I really like Chinese ____.
+
+我很喜欢中国的____。
+wǒ hěn xǐ huān zhōng guó de ____.
+
+Options:
+
+1. 安全
+ān quán
+safe
+
+2. 文化
+wén huà
+culture
+
+3. 交通
+jiāo tōng
+traffic
+
+4. 方便
+fāng biàn
+convenient
+
+5. 堵车
+dǔ chē
+traffic jam`
+},
+
+{
+type:4,
+front:"这个城市晚上____吗？",
+back:`Is this city ____ at night?
+
+这个城市晚上____吗？
+zhè ge chéng shì wǎn shang ____ ma?
+
+Options:
+
+1. 交通
+jiāo tōng
+traffic
+
+2. 方便
+fāng biàn
+convenient
+
+3. 堵车
+dǔ chē
+traffic jam
+
+4. 安全
+ān quán
+safe
+
+5. 商店
+shāng diàn
+shop`
+},
+
+{
+type:4,
+front:"这里的____很方便。",
+back:`The ____ here is very convenient.
+
+这里的____很方便。
+zhè lǐ de ____ hěn fāng biàn.
+
+Options:
+
+1. 方便
+fāng biàn
+convenient
+
+2. 堵车
+dǔ chē
+traffic jam
+
+3. 商店
+shāng diàn
+shop
+
+4. 游客
+yóu kè
+tourist
+
+5. 交通
+jiāo tōng
+traffic`
+},
+
+{
+type:4,
+front:"坐地铁去机场很____。",
+back:`Taking the subway to the airport is very ____.
+
+坐地铁去机场很____。
+zuò dì tiě qù jī chǎng hěn ____.
+
+Options:
+
+1. 方便
+fāng biàn
+convenient
+
+2. 堵车
+dǔ chē
+traffic jam
+
+3. 商店
+shāng diàn
+shop
+
+4. 游客
+yóu kè
+tourist
+
+5. 热情
+rè qíng
+hospitable`
+},
+
+{
+type:4,
+front:"上班的时候常常____。",
+back:`During commute time, there's often a ____.
+
+上班的时候常常____。
+shàng bān de shí hou cháng cháng ____.
+
+Options:
+
+1. 商店
+shāng diàn
+shop
+
+2. 游客
+yóu kè
+tourist
+
+3. 堵车
+dǔ chē
+traffic jam
+
+4. 热情
+rè qíng
+hospitable
+
+5. 城市
+chéng shì
+city`
+},
+
+{
+type:4,
+front:"那家____卖什么？",
+back:`What does that ____ sell?
+
+那家____卖什么？
+nà jiā ____ mài shén me?
+
+Options:
+
+1. 游客
+yóu kè
+tourist
+
+2. 商店
+shāng diàn
+shop
+
+3. 热情
+rè qíng
+hospitable
+
+4. 城市
+chéng shì
+city
+
+5. 觉得
+jué de
+to think`
+},
+
+{
+type:4,
+front:"夏天这里有很多____。",
+back:`In summer there are many ____ here.
+
+夏天这里有很多____。
+xià tiān zhè lǐ yǒu hěn duō ____.
+
+Options:
+
+1. 热情
+rè qíng
+hospitable
+
+2. 城市
+chéng shì
+city
+
+3. 觉得
+jué de
+to think
+
+4. 游客
+yóu kè
+tourist
+
+5. 可以
+kě yǐ
+can`
+},
+
+{
+type:4,
+front:"这里的人很____。",
+back:`The people here are very ____.
+
+这里的人很____。
+zhè lǐ de rén hěn ____.
+
+Options:
+
+1. 城市
+chéng shì
+city
+
+2. 觉得
+jué de
+to think
+
+3. 可以
+kě yǐ
+can
+
+4. 已经
+yǐ jīng
+already
+
+5. 热情
+rè qíng
+hospitable`
+},
+
+{
+type:4,
+front:"天津是一个很大的____。",
+back:`Tianjin is a very big ____.
+
+天津是一个很大的____。
+tiān jīn shì yí gè hěn dà de ____.
+
+Options:
+
+1. 城市
+chéng shì
+city
+
+2. 觉得
+jué de
+to think
+
+3. 可以
+kě yǐ
+can
+
+4. 已经
+yǐ jīng
+already
+
+5. 耐心
+nài xīn
+patience`
+},
+
+{
+type:4,
+front:"你____天津怎么样？",
+back:`What do you ____ about Tianjin?
+
+你____天津怎么样？
+nǐ ____ tiān jīn zěn me yàng?
+
+Options:
+
+1. 可以
+kě yǐ
+can
+
+2. 已经
+yǐ jīng
+already
+
+3. 觉得
+jué de
+to think
+
+4. 耐心
+nài xīn
+patience
+
+5. 一样
+yí yàng
+the same`
+},
+
+{
+type:4,
+front:"我____在公园跑步。",
+back:`I ____ run in the park.
+
+我____在公园跑步。
+wǒ ____ zài gōng yuán pǎo bù.
+
+Options:
+
+1. 已经
+yǐ jīng
+already
+
+2. 可以
+kě yǐ
+can
+
+3. 耐心
+nài xīn
+patience
+
+4. 一样
+yí yàng
+the same
+
+5. 哪儿
+nǎr
+where`
+},
+
+{
+type:4,
+front:"我____熟悉这里的路了。",
+back:`I am ____ familiar with the roads here.
+
+我____熟悉这里的路了。
+wǒ ____ shú xī zhè lǐ de lù le.
+
+Options:
+
+1. 耐心
+nài xīn
+patience
+
+2. 一样
+yí yàng
+the same
+
+3. 哪儿
+nǎr
+where
+
+4. 已经
+yǐ jīng
+already
+
+5. 文化
+wén huà
+culture`
+},
+
+{
+type:4,
+front:"学中文需要____。",
+back:`Learning Chinese requires ____.
+
+学中文需要____。
+xué zhōng wén xū yào ____.
+
+Options:
+
+1. 一样
+yí yàng
+the same
+
+2. 哪儿
+nǎr
+where
+
+3. 文化
+wén huà
+culture
+
+4. 安全
+ān quán
+safe
+
+5. 耐心
+nài xīn
+patience`
+},
+
+]);
