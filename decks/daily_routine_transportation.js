20a148ba6565ae917d2eef8a52f1c41a1a49bdb1
Shaun Phillips <smphillips@alumni.york.ac.uk>
2026-06-20 12:29:05 +0200

    add next lesson deck

---
+++ decks/daily_routine_transportation.js
@@ -0,0 +1,483 @@
+registerDeck("Daily Routine & Transportation", [
+
+{
+type:1,
+front:"同事",
+back:`同事\ntóng shì\nCo-worker`
+},
+{
+type:2,
+front:"Co-worker",
+back:`同事\ntóng shì\nCo-worker`
+},
+{
+type:3,
+front:"tong shi",
+back:`同事\ntóng shì\nCo-worker`
+},
+
+{
+type:1,
+front:"起床",
+back:`起床\nqǐ chuáng\nGet up (get out of bed)`
+},
+{
+type:2,
+front:"Get up (get out of bed)",
+back:`起床\nqǐ chuáng\nGet up (get out of bed)`
+},
+{
+type:3,
+front:"qi chuang",
+back:`起床\nqǐ chuáng\nGet up (get out of bed)`
+},
+
+{
+type:1,
+front:"睡觉",
+back:`睡觉\nshuì jiào\nGo to bed; sleep`
+},
+{
+type:2,
+front:"Go to bed; sleep",
+back:`睡觉\nshuì jiào\nGo to bed; sleep`
+},
+{
+type:3,
+front:"shui jiao",
+back:`睡觉\nshuì jiào\nGo to bed; sleep`
+},
+
+{
+type:1,
+front:"公司",
+back:`公司\ngōng sī\nCompany`
+},
+{
+type:2,
+front:"Company",
+back:`公司\ngōng sī\nCompany`
+},
+{
+type:3,
+front:"gong si",
+back:`公司\ngōng sī\nCompany`
+},
+
+{
+type:1,
+front:"经理",
+back:`经理\njīng lǐ\nManager`
+},
+{
+type:2,
+front:"Manager",
+back:`经理\njīng lǐ\nManager`
+},
+{
+type:3,
+front:"jing li",
+back:`经理\njīng lǐ\nManager`
+},
+
+{
+type:1,
+front:"旅游",
+back:`旅游\nlǚ yóu\nTourism; travel`
+},
+{
+type:2,
+front:"Tourism; travel",
+back:`旅游\nlǚ yóu\nTourism; travel`
+},
+{
+type:3,
+front:"lu you",
+back:`旅游\nlǚ yóu\nTourism; travel`
+},
+
+{
+type:1,
+front:"妻子",
+back:`妻子\nqī zi\nWife`
+},
+{
+type:2,
+front:"Wife",
+back:`妻子\nqī zi\nWife`
+},
+{
+type:3,
+front:"qi zi",
+back:`妻子\nqī zi\nWife`
+},
+
+{
+type:1,
+front:"告诉",
+back:`告诉\ngào sù\nTell (someone something)`
+},
+{
+type:2,
+front:"Tell (someone something)",
+back:`告诉\ngào sù\nTell (someone something)`
+},
+{
+type:3,
+front:"gao su",
+back:`告诉\ngào sù\nTell (someone something)`
+},
+
+{
+type:1,
+front:"国家",
+back:`国家\nguó jiā\nCountry`
+},
+{
+type:2,
+front:"Country",
+back:`国家\nguó jiā\nCountry`
+},
+{
+type:3,
+front:"guo jia",
+back:`国家\nguó jiā\nCountry`
+},
+
+{
+type:1,
+front:"骑",
+back:`骑\nqí\nRide (bike, motorcycle, horse, etc.)`
+},
+{
+type:2,
+front:"Ride (bike, motorcycle, horse, etc.)",
+back:`骑\nqí\nRide (bike, motorcycle, horse, etc.)`
+},
+{
+type:3,
+front:"qi",
+back:`骑\nqí\nRide (bike, motorcycle, horse, etc.)`
+},
+
+{
+type:1,
+front:"自行车",
+back:`自行车\nzì xíng chē\nBicycle`
+},
+{
+type:2,
+front:"Bicycle",
+back:`自行车\nzì xíng chē\nBicycle`
+},
+{
+type:3,
+front:"zi xing che",
+back:`自行车\nzì xíng chē\nBicycle`
+},
+
+{
+type:1,
+front:"摩托车",
+back:`摩托车\nmó tuō chē\nMotorcycle`
+},
+{
+type:2,
+front:"Motorcycle",
+back:`摩托车\nmó tuō chē\nMotorcycle`
+},
+{
+type:3,
+front:"mo tuo che",
+back:`摩托车\nmó tuō chē\nMotorcycle`
+},
+
+{
+type:1,
+front:"坐",
+back:`坐\nzuò\nSit; take (transport)`
+},
+{
+type:2,
+front:"Sit; take (transport)",
+back:`坐\nzuò\nSit; take (transport)`
+},
+{
+type:3,
+front:"zuo",
+back:`坐\nzuò\nSit; take (transport)`
+},
+
+{
+type:1,
+front:"地铁",
+back:`地铁\ndì tiě\nSubway; metro`
+},
+{
+type:2,
+front:"Subway; metro",
+back:`地铁\ndì tiě\nSubway; metro`
+},
+{
+type:3,
+front:"di tie",
+back:`地铁\ndì tiě\nSubway; metro`
+},
+
+{
+type:1,
+front:"飞机",
+back:`飞机\nfēi jī\nAirplane`
+},
+{
+type:2,
+front:"Airplane",
+back:`飞机\nfēi jī\nAirplane`
+},
+{
+type:3,
+front:"fei ji",
+back:`飞机\nfēi jī\nAirplane`
+},
+
+{
+type:1,
+front:"火车",
+back:`火车\nhuǒ chē\nTrain`
+},
+{
+type:2,
+front:"Train",
+back:`火车\nhuǒ chē\nTrain`
+},
+{
+type:3,
+front:"huo che",
+back:`火车\nhuǒ chē\nTrain`
+},
+
+{
+type:1,
+front:"出租车",
+back:`出租车\nchū zū chē\nTaxi`
+},
+{
+type:2,
+front:"Taxi",
+back:`出租车\nchū zū chē\nTaxi`
+},
+{
+type:3,
+front:"chu zu che",
+back:`出租车\nchū zū chē\nTaxi`
+},
+
+{
+type:1,
+front:"打的",
+back:`打的\ndǎ dī\nTake a taxi`
+},
+{
+type:2,
+front:"Take a taxi",
+back:`打的\ndǎ dī\nTake a taxi`
+},
+{
+type:3,
+front:"da di",
+back:`打的\ndǎ dī\nTake a taxi`
+},
+
+{
+type:1,
+front:"开车",
+back:`开车\nkāi chē\nDrive`
+},
+{
+type:2,
+front:"Drive",
+back:`开车\nkāi chē\nDrive`
+},
+{
+type:3,
+front:"kai che",
+back:`开车\nkāi chē\nDrive`
+},
+
+{
+type:1,
+front:"上班",
+back:`上班\nshàng bān\nGo to work; be at work`
+},
+{
+type:2,
+front:"Go to work; be at work",
+back:`上班\nshàng bān\nGo to work; be at work`
+},
+{
+type:3,
+front:"shang ban",
+back:`上班\nshàng bān\nGo to work; be at work`
+},
+
+{
+type:1,
+front:"下班",
+back:`下班\nxià bān\nGet off work`
+},
+{
+type:2,
+front:"Get off work",
+back:`下班\nxià bān\nGet off work`
+},
+{
+type:3,
+front:"xia ban",
+back:`下班\nxià bān\nGet off work`
+},
+
+{
+type:1,
+front:"走",
+back:`走\nzǒu\nGo; leave; walk`
+},
+{
+type:2,
+front:"Go; leave; walk",
+back:`走\nzǒu\nGo; leave; walk`
+},
+{
+type:3,
+front:"zou",
+back:`走\nzǒu\nGo; leave; walk`
+},
+
+{
+type:1,
+front:"早上",
+back:`早上\nzǎo shàng\nMorning`
+},
+{
+type:2,
+front:"Morning",
+back:`早上\nzǎo shàng\nMorning`
+},
+{
+type:3,
+front:"zao shang",
+back:`早上\nzǎo shàng\nMorning`
+},
+
+{
+type:1,
+front:"下午",
+back:`下午\nxià wǔ\nAfternoon`
+},
+{
+type:2,
+front:"Afternoon",
+back:`下午\nxià wǔ\nAfternoon`
+},
+{
+type:3,
+front:"xia wu",
+back:`下午\nxià wǔ\nAfternoon`
+},
+
+{
+type:1,
+front:"晚上",
+back:`晚上\nwǎn shàng\nEvening; night`
+},
+{
+type:2,
+front:"Evening; night",
+back:`晚上\nwǎn shàng\nEvening; night`
+},
+{
+type:3,
+front:"wan shang",
+back:`晚上\nwǎn shàng\nEvening; night`
+},
+
+{
+type:1,
+front:"以前",
+back:`以前\nyǐ qián\nBefore; ago`
+},
+{
+type:2,
+front:"Before; ago",
+back:`以前\nyǐ qián\nBefore; ago`
+},
+{
+type:3,
+front:"yi qian",
+back:`以前\nyǐ qián\nBefore; ago`
+},
+
+{
+type:1,
+front:"手表",
+back:`手表\nshǒu biǎo\nWristwatch`
+},
+{
+type:2,
+front:"Wristwatch",
+back:`手表\nshǒu biǎo\nWristwatch`
+},
+{
+type:3,
+front:"shou biao",
+back:`手表\nshǒu biǎo\nWristwatch`
+},
+
+{
+type:1,
+front:"迟到",
+back:`迟到\nchí dào\nBe late; arrive late`
+},
+{
+type:2,
+front:"Be late; arrive late",
+back:`迟到\nchí dào\nBe late; arrive late`
+},
+{
+type:3,
+front:"chi dao",
+back:`迟到\nchí dào\nBe late; arrive late`
+},
+
+{
+type:1,
+front:"公共汽车",
+back:`公共汽车\ngōng gòng qì chē\nBus`
+},
+{
+type:2,
+front:"Bus",
+back:`公共汽车\ngōng gòng qì chē\nBus`
+},
+{
+type:3,
+front:"gong gong qi che",
+back:`公共汽车\ngōng gòng qì chē\nBus`
+},
+
+{
+type:1,
+front:"什么时候",
+back:`什么时候\nshén me shí hou\nWhen`
+},
+{
+type:2,
+front:"When",
+back:`什么时候\nshén me shí hou\nWhen`
+},
+{
+type:3,
+front:"shen me shi hou",
+back:`什么时候\nshén me shí hou\nWhen`
+},
+
+]);
