fcff506cb57ce00e4a4cafe003c723fe9aaf3e6b
Shaun Phillips <smphillips@alumni.york.ac.uk>
2026-06-21 21:09:58 +0200

    add lesson 3 deck

---
+++ decks/exams_career_life.js
@@ -0,0 +1,483 @@
+registerDeck("Exams, Career & Life", [
+
+{
+type:1,
+front:"考试",
+back:`考试\nkǎo shì\nExam`
+},
+{
+type:2,
+front:"Exam",
+back:`考试\nkǎo shì\nExam`
+},
+{
+type:3,
+front:"kao shi",
+back:`考试\nkǎo shì\nExam`
+},
+
+{
+type:1,
+front:"口试",
+back:`口试\nkǒu shì\nOral exam`
+},
+{
+type:2,
+front:"Oral exam",
+back:`口试\nkǒu shì\nOral exam`
+},
+{
+type:3,
+front:"kou shi",
+back:`口试\nkǒu shì\nOral exam`
+},
+
+{
+type:1,
+front:"笔试",
+back:`笔试\nbǐ shì\nWritten exam`
+},
+{
+type:2,
+front:"Written exam",
+back:`笔试\nbǐ shì\nWritten exam`
+},
+{
+type:3,
+front:"bi shi",
+back:`笔试\nbǐ shì\nWritten exam`
+},
+
+{
+type:1,
+front:"面试",
+back:`面试\nmiàn shì\nJob interview`
+},
+{
+type:2,
+front:"Job interview",
+back:`面试\nmiàn shì\nJob interview`
+},
+{
+type:3,
+front:"mian shi",
+back:`面试\nmiàn shì\nJob interview`
+},
+
+{
+type:1,
+front:"报纸",
+back:`报纸\nbào zhǐ\nNewspaper`
+},
+{
+type:2,
+front:"Newspaper",
+back:`报纸\nbào zhǐ\nNewspaper`
+},
+{
+type:3,
+front:"bao zhi",
+back:`报纸\nbào zhǐ\nNewspaper`
+},
+
+{
+type:1,
+front:"餐馆",
+back:`餐馆\ncān guǎn\nRestaurant`
+},
+{
+type:2,
+front:"Restaurant",
+back:`餐馆\ncān guǎn\nRestaurant`
+},
+{
+type:3,
+front:"can guan",
+back:`餐馆\ncān guǎn\nRestaurant`
+},
+
+{
+type:1,
+front:"长",
+back:`长\ncháng\nLong`
+},
+{
+type:2,
+front:"Long",
+back:`长\ncháng\nLong`
+},
+{
+type:3,
+front:"chang",
+back:`长\ncháng\nLong`
+},
+
+{
+type:1,
+front:"城市",
+back:`城市\nchéng shì\nCity`
+},
+{
+type:2,
+front:"City",
+back:`城市\nchéng shì\nCity`
+},
+{
+type:3,
+front:"cheng shi",
+back:`城市\nchéng shì\nCity`
+},
+
+{
+type:1,
+front:"地中海",
+back:`地中海\ndì zhōng hǎi\nMediterranean Sea`
+},
+{
+type:2,
+front:"Mediterranean Sea",
+back:`地中海\ndì zhōng hǎi\nMediterranean Sea`
+},
+{
+type:3,
+front:"di zhong hai",
+back:`地中海\ndì zhōng hǎi\nMediterranean Sea`
+},
+
+{
+type:1,
+front:"翻译",
+back:`翻译\nfān yì\nTranslate; translation`
+},
+{
+type:2,
+front:"Translate; translation",
+back:`翻译\nfān yì\nTranslate; translation`
+},
+{
+type:3,
+front:"fan yi",
+back:`翻译\nfān yì\nTranslate; translation`
+},
+
+{
+type:1,
+front:"分钟",
+back:`分钟\nfēn zhōng\nMinute`
+},
+{
+type:2,
+front:"Minute",
+back:`分钟\nfēn zhōng\nMinute`
+},
+{
+type:3,
+front:"fen zhong",
+back:`分钟\nfēn zhōng\nMinute`
+},
+
+{
+type:1,
+front:"服务员",
+back:`服务员\nfú wù yuán\nWaiter; waitress`
+},
+{
+type:2,
+front:"Waiter; waitress",
+back:`服务员\nfú wù yuán\nWaiter; waitress`
+},
+{
+type:3,
+front:"fu wu yuan",
+back:`服务员\nfú wù yuán\nWaiter; waitress`
+},
+
+{
+type:1,
+front:"专业",
+back:`专业\nzhuān yè\nMajor; specialty`
+},
+{
+type:2,
+front:"Major; specialty",
+back:`专业\nzhuān yè\nMajor; specialty`
+},
+{
+type:3,
+front:"zhuan ye",
+back:`专业\nzhuān yè\nMajor; specialty`
+},
+
+{
+type:1,
+front:"法律",
+back:`法律\nfǎ lǜ\nLaw`
+},
+{
+type:2,
+front:"Law",
+back:`法律\nfǎ lǜ\nLaw`
+},
+{
+type:3,
+front:"fa lv",
+back:`法律\nfǎ lǜ\nLaw`
+},
+
+{
+type:1,
+front:"经济",
+back:`经济\njīng jì\nEconomy`
+},
+{
+type:2,
+front:"Economy",
+back:`经济\njīng jì\nEconomy`
+},
+{
+type:3,
+front:"jing ji",
+back:`经济\njīng jì\nEconomy`
+},
+
+{
+type:1,
+front:"复习",
+back:`复习\nfù xí\nReview; revise`
+},
+{
+type:2,
+front:"Review; revise",
+back:`复习\nfù xí\nReview; revise`
+},
+{
+type:3,
+front:"fu xi",
+back:`复习\nfù xí\nReview; revise`
+},
+
+{
+type:1,
+front:"经常",
+back:`经常\njīng cháng\nOften`
+},
+{
+type:2,
+front:"Often",
+back:`经常\njīng cháng\nOften`
+},
+{
+type:3,
+front:"jing chang",
+back:`经常\njīng cháng\nOften`
+},
+
+{
+type:1,
+front:"毕业",
+back:`毕业\nbì yè\nGraduate`
+},
+{
+type:2,
+front:"Graduate",
+back:`毕业\nbì yè\nGraduate`
+},
+{
+type:3,
+front:"bi ye",
+back:`毕业\nbì yè\nGraduate`
+},
+
+{
+type:1,
+front:"简历",
+back:`简历\njiǎn lì\nCV; resume`
+},
+{
+type:2,
+front:"CV; resume",
+back:`简历\njiǎn lì\nCV; resume`
+},
+{
+type:3,
+front:"jian li",
+back:`简历\njiǎn lì\nCV; resume`
+},
+
+{
+type:1,
+front:"留学生",
+back:`留学生\nliú xué shēng\nInternational student`
+},
+{
+type:2,
+front:"International student",
+back:`留学生\nliú xué shēng\nInternational student`
+},
+{
+type:3,
+front:"liu xue sheng",
+back:`留学生\nliú xué shēng\nInternational student`
+},
+
+{
+type:1,
+front:"历史",
+back:`历史\nlì shǐ\nHistory`
+},
+{
+type:2,
+front:"History",
+back:`历史\nlì shǐ\nHistory`
+},
+{
+type:3,
+front:"li shi",
+back:`历史\nlì shǐ\nHistory`
+},
+
+{
+type:1,
+front:"生活",
+back:`生活\nshēng huó\nLife; live`
+},
+{
+type:2,
+front:"Life; live",
+back:`生活\nshēng huó\nLife; live`
+},
+{
+type:3,
+front:"sheng huo",
+back:`生活\nshēng huó\nLife; live`
+},
+
+{
+type:1,
+front:"熟悉",
+back:`熟悉\nshú xī\nBe familiar with`
+},
+{
+type:2,
+front:"Be familiar with",
+back:`熟悉\nshú xī\nBe familiar with`
+},
+{
+type:3,
+front:"shu xi",
+back:`熟悉\nshú xī\nBe familiar with`
+},
+
+{
+type:1,
+front:"听说",
+back:`听说\ntīng shuō\nHear of; it is said that`
+},
+{
+type:2,
+front:"Hear of; it is said that",
+back:`听说\ntīng shuō\nHear of; it is said that`
+},
+{
+type:3,
+front:"ting shuo",
+back:`听说\ntīng shuō\nHear of; it is said that`
+},
+
+{
+type:1,
+front:"辛苦",
+back:`辛苦\nxīn kǔ\nHard; tiring`
+},
+{
+type:2,
+front:"Hard; tiring",
+back:`辛苦\nxīn kǔ\nHard; tiring`
+},
+{
+type:3,
+front:"xin ku",
+back:`辛苦\nxīn kǔ\nHard; tiring`
+},
+
+{
+type:1,
+front:"小时",
+back:`小时\nxiǎo shí\nHour`
+},
+{
+type:2,
+front:"Hour",
+back:`小时\nxiǎo shí\nHour`
+},
+{
+type:3,
+front:"xiao shi",
+back:`小时\nxiǎo shí\nHour`
+},
+
+{
+type:1,
+front:"以后",
+back:`以后\nyǐ hòu\nAfter; later`
+},
+{
+type:2,
+front:"After; later",
+back:`以后\nyǐ hòu\nAfter; later`
+},
+{
+type:3,
+front:"yi hou",
+back:`以后\nyǐ hòu\nAfter; later`
+},
+
+{
+type:1,
+front:"有名",
+back:`有名\nyǒu míng\nFamous`
+},
+{
+type:2,
+front:"Famous",
+back:`有名\nyǒu míng\nFamous`
+},
+{
+type:3,
+front:"you ming",
+back:`有名\nyǒu míng\nFamous`
+},
+
+{
+type:1,
+front:"找",
+back:`找\nzhǎo\nLook for; find`
+},
+{
+type:2,
+front:"Look for; find",
+back:`找\nzhǎo\nLook for; find`
+},
+{
+type:3,
+front:"zhao",
+back:`找\nzhǎo\nLook for; find`
+},
+
+{
+type:1,
+front:"准备",
+back:`准备\nzhǔn bèi\nPrepare; get ready`
+},
+{
+type:2,
+front:"Prepare; get ready",
+back:`准备\nzhǔn bèi\nPrepare; get ready`
+},
+{
+type:3,
+front:"zhun bei",
+back:`准备\nzhǔn bèi\nPrepare; get ready`
+},
+
+]);
