e6f03d1e96a0af97434092aac9db13bb2ef316cc
Shaun Phillips <smphillips@alumni.york.ac.uk>
2026-06-25 08:45:49 +0200

    New decks

---
+++ decks/sports_comparisons.js
@@ -0,0 +1,323 @@
+registerDeck("Sports & Comparisons", [
+
+{
+type:1,
+front:'比',
+back:`比\nbǐ\nCompared to (comparison particle)`
+},
+{
+type:2,
+front:'Compared to (comparison particle)',
+back:`比\nbǐ\nCompared to (comparison particle)`
+},
+{
+type:3,
+front:'bi',
+back:`比\nbǐ\nCompared to (comparison particle)`
+},
+
+{
+type:1,
+front:'更',
+back:`更\ngèng\nEven more; still more`
+},
+{
+type:2,
+front:'Even more; still more',
+back:`更\ngèng\nEven more; still more`
+},
+{
+type:3,
+front:'geng',
+back:`更\ngèng\nEven more; still more`
+},
+
+{
+type:1,
+front:'游泳',
+back:`游泳\nyóu yǒng\nTo swim`
+},
+{
+type:2,
+front:'To swim',
+back:`游泳\nyóu yǒng\nTo swim`
+},
+{
+type:3,
+front:'you yong',
+back:`游泳\nyóu yǒng\nTo swim`
+},
+
+{
+type:1,
+front:'差不多',
+back:`差不多\nchā bu duō\nApproximately; almost`
+},
+{
+type:2,
+front:'Approximately; almost',
+back:`差不多\nchā bu duō\nApproximately; almost`
+},
+{
+type:3,
+front:'cha bu duo',
+back:`差不多\nchā bu duō\nApproximately; almost`
+},
+
+{
+type:1,
+front:'左右',
+back:`左右\nzuǒ yòu\nAround; approximately (time/amount)`
+},
+{
+type:2,
+front:'Around; approximately (time/amount)',
+back:`左右\nzuǒ yòu\nAround; approximately (time/amount)`
+},
+{
+type:3,
+front:'zuo you',
+back:`左右\nzuǒ yòu\nAround; approximately (time/amount)`
+},
+
+{
+type:1,
+front:'赶上',
+back:`赶上\ngǎn shàng\nTo catch (a bus, train, etc.)`
+},
+{
+type:2,
+front:'To catch (a bus, train, etc.)',
+back:`赶上\ngǎn shàng\nTo catch (a bus, train, etc.)`
+},
+{
+type:3,
+front:'gan shang',
+back:`赶上\ngǎn shàng\nTo catch (a bus, train, etc.)`
+},
+
+{
+type:1,
+front:'沙滩',
+back:`沙滩\nshā tān\nSandy beach`
+},
+{
+type:2,
+front:'Sandy beach',
+back:`沙滩\nshā tān\nSandy beach`
+},
+{
+type:3,
+front:'sha tan',
+back:`沙滩\nshā tān\nSandy beach`
+},
+
+{
+type:1,
+front:'看见',
+back:`看见\nkàn jiàn\nTo see; to catch sight of`
+},
+{
+type:2,
+front:'To see; to catch sight of',
+back:`看见\nkàn jiàn\nTo see; to catch sight of`
+},
+{
+type:3,
+front:'kan jian',
+back:`看见\nkàn jiàn\nTo see; to catch sight of`
+},
+
+{
+type:1,
+front:'看到',
+back:`看到\nkàn dào\nTo see; to manage to see (result)`
+},
+{
+type:2,
+front:'To see; to manage to see (result)',
+back:`看到\nkàn dào\nTo see; to manage to see (result)`
+},
+{
+type:3,
+front:'kan dao',
+back:`看到\nkàn dào\nTo see; to manage to see (result)`
+},
+
+{
+type:1,
+front:'篮球',
+back:`篮球\nlán qiú\nBasketball`
+},
+{
+type:2,
+front:'Basketball',
+back:`篮球\nlán qiú\nBasketball`
+},
+{
+type:3,
+front:'lan qiu',
+back:`篮球\nlán qiú\nBasketball`
+},
+
+{
+type:1,
+front:'足球',
+back:`足球\nzú qiú\nFootball; soccer`
+},
+{
+type:2,
+front:'Football; soccer',
+back:`足球\nzú qiú\nFootball; soccer`
+},
+{
+type:3,
+front:'zu qiu',
+back:`足球\nzú qiú\nFootball; soccer`
+},
+
+{
+type:1,
+front:'虽然',
+back:`虽然\nsuī rán\nAlthough; even though`
+},
+{
+type:2,
+front:'Although; even though',
+back:`虽然\nsuī rán\nAlthough; even though`
+},
+{
+type:3,
+front:'sui ran',
+back:`虽然\nsuī rán\nAlthough; even though`
+},
+
+{
+type:1,
+front:'世界杯',
+back:`世界杯\nshì jiè bēi\nWorld Cup`
+},
+{
+type:2,
+front:'World Cup',
+back:`世界杯\nshì jiè bēi\nWorld Cup`
+},
+{
+type:3,
+front:'shi jie bei',
+back:`世界杯\nshì jiè bēi\nWorld Cup`
+},
+
+{
+type:1,
+front:'比赛',
+back:`比赛\nbǐ sài\nMatch; competition`
+},
+{
+type:2,
+front:'Match; competition',
+back:`比赛\nbǐ sài\nMatch; competition`
+},
+{
+type:3,
+front:'bi sai',
+back:`比赛\nbǐ sài\nMatch; competition`
+},
+
+{
+type:1,
+front:'运动',
+back:`运动\nyùn dòng\nSport; exercise`
+},
+{
+type:2,
+front:'Sport; exercise',
+back:`运动\nyùn dòng\nSport; exercise`
+},
+{
+type:3,
+front:'yun dong',
+back:`运动\nyùn dòng\nSport; exercise`
+},
+
+{
+type:1,
+front:'健康',
+back:`健康\njiàn kāng\nHealthy; health`
+},
+{
+type:2,
+front:'Healthy; health',
+back:`健康\njiàn kāng\nHealthy; health`
+},
+{
+type:3,
+front:'jian kang',
+back:`健康\njiàn kāng\nHealthy; health`
+},
+
+{
+type:1,
+front:'保持',
+back:`保持\nbǎo chí\nTo maintain; to keep up`
+},
+{
+type:2,
+front:'To maintain; to keep up',
+back:`保持\nbǎo chí\nTo maintain; to keep up`
+},
+{
+type:3,
+front:'bao chi',
+back:`保持\nbǎo chí\nTo maintain; to keep up`
+},
+
+{
+type:1,
+front:'一般',
+back:`一般\nyī bān\nOrdinary; in general; generally`
+},
+{
+type:2,
+front:'Ordinary; in general; generally',
+back:`一般\nyī bān\nOrdinary; in general; generally`
+},
+{
+type:3,
+front:'yi ban',
+back:`一般\nyī bān\nOrdinary; in general; generally`
+},
+
+{
+type:1,
+front:'发呆',
+back:`发呆\nfā dāi\nTo space out; to stare blankly`
+},
+{
+type:2,
+front:'To space out; to stare blankly',
+back:`发呆\nfā dāi\nTo space out; to stare blankly`
+},
+{
+type:3,
+front:'fa dai',
+back:`发呆\nfā dāi\nTo space out; to stare blankly`
+},
+
+{
+type:1,
+front:'北边',
+back:`北边\nběi biān\nNorth side; northern area`
+},
+{
+type:2,
+front:'North side; northern area',
+back:`北边\nběi biān\nNorth side; northern area`
+},
+{
+type:3,
+front:'bei bian',
+back:`北边\nběi biān\nNorth side; northern area`
+},
+
+]);
---
+++ decks/work_city_life.js
@@ -0,0 +1,223 @@
+registerDeck("Work & City Life", [
+
+{
+type:1,
+front:'老板',
+back:`老板\nlǎo bǎn\nBoss`
+},
+{
+type:2,
+front:'Boss',
+back:`老板\nlǎo bǎn\nBoss`
+},
+{
+type:3,
+front:'lao ban',
+back:`老板\nlǎo bǎn\nBoss`
+},
+
+{
+type:1,
+front:'海边',
+back:`海边\nhǎi biān\nSeaside; beach`
+},
+{
+type:2,
+front:'Seaside; beach',
+back:`海边\nhǎi biān\nSeaside; beach`
+},
+{
+type:3,
+front:'hai bian',
+back:`海边\nhǎi biān\nSeaside; beach`
+},
+
+{
+type:1,
+front:'开会',
+back:`开会\nkāi huì\nTo have a meeting`
+},
+{
+type:2,
+front:'To have a meeting',
+back:`开会\nkāi huì\nTo have a meeting`
+},
+{
+type:3,
+front:'kai hui',
+back:`开会\nkāi huì\nTo have a meeting`
+},
+
+{
+type:1,
+front:'附近',
+back:`附近\nfù jìn\nNearby; in the vicinity`
+},
+{
+type:2,
+front:'Nearby; in the vicinity',
+back:`附近\nfù jìn\nNearby; in the vicinity`
+},
+{
+type:3,
+front:'fu jin',
+back:`附近\nfù jìn\nNearby; in the vicinity`
+},
+
+{
+type:1,
+front:'办公室',
+back:`办公室\nbàn gōng shì\nOffice`
+},
+{
+type:2,
+front:'Office',
+back:`办公室\nbàn gōng shì\nOffice`
+},
+{
+type:3,
+front:'ban gong shi',
+back:`办公室\nbàn gōng shì\nOffice`
+},
+
+{
+type:1,
+front:'部门',
+back:`部门\nbù mén\nDepartment`
+},
+{
+type:2,
+front:'Department',
+back:`部门\nbù mén\nDepartment`
+},
+{
+type:3,
+front:'bu men',
+back:`部门\nbù mén\nDepartment`
+},
+
+{
+type:1,
+front:'驾照',
+back:`驾照\njià zhào\nDriver's licence`
+},
+{
+type:2,
+front:"Driver's licence",
+back:`驾照\njià zhào\nDriver's licence`
+},
+{
+type:3,
+front:'jia zhao',
+back:`驾照\njià zhào\nDriver's licence`
+},
+
+{
+type:1,
+front:'年轻人',
+back:`年轻人\nnián qīng rén\nYoung person`
+},
+{
+type:2,
+front:'Young person',
+back:`年轻人\nnián qīng rén\nYoung person`
+},
+{
+type:3,
+front:'nian qing ren',
+back:`年轻人\nnián qīng rén\nYoung person`
+},
+
+{
+type:1,
+front:'听力',
+back:`听力\ntīng lì\nListening comprehension`
+},
+{
+type:2,
+front:'Listening comprehension',
+back:`听力\ntīng lì\nListening comprehension`
+},
+{
+type:3,
+front:'ting li',
+back:`听力\ntīng lì\nListening comprehension`
+},
+
+{
+type:1,
+front:'科技',
+back:`科技\nkē jì\nTechnology`
+},
+{
+type:2,
+front:'Technology',
+back:`科技\nkē jì\nTechnology`
+},
+{
+type:3,
+front:'ke ji',
+back:`科技\nkē jì\nTechnology`
+},
+
+{
+type:4,
+front:'明天我要去____。',
+back:`Tomorrow I need to go to ____.\n\n明天我要去____。\nmíng tiān wǒ yào qù ____.\n\nOptions:\n\n1. 开会\nkāi huì\nhave a meeting\n\n2. 上班\nshàng bān\ngo to work\n\n3. 办公室\nbàn gōng shì\nthe office\n\n4. 海边\nhǎi biān\nthe seaside\n\n5. 公司\ngōng sī\nthe company`
+},
+
+{
+type:4,
+front:'我的家离公司很____。',
+back:`My home is very ____ from the company.\n\n我的家离公司很____。\nwǒ de jiā lí gōng sī hěn ____.\n\nOptions:\n\n1. 近\njìn\nclose\n\n2. 远\nyuǎn\nfar\n\n3. 方便\nfāng biàn\nconvenient\n\n4. 好\nhǎo\ngood\n\n5. 快\nkuài\nquick`
+},
+
+{
+type:4,
+front:'我和____一起去开会。',
+back:`I'm going to the meeting together with my ____.\n\n我和____一起去开会。\nwǒ hé ____ yī qǐ qù kāi huì.\n\nOptions:\n\n1. 老板\nlǎo bǎn\nboss\n\n2. 同事\ntóng shì\ncolleague\n\n3. 经理\njīng lǐ\nmanager\n\n4. 朋友\npéng yǒu\nfriend\n\n5. 老师\nlǎo shī\nteacher`
+},
+
+{
+type:4,
+front:'周末我想去____玩儿。',
+back:`At the weekend I want to go to the ____ and have fun.\n\n周末我想去____玩儿。\nzhōu mò wǒ xiǎng qù ____ wán er.\n\nOptions:\n\n1. 海边\nhǎi biān\nseaside\n\n2. 城市\nchéng shì\ncity\n\n3. 附近\nfù jìn\nnearby area\n\n4. 公园\ngōng yuán\npark\n\n5. 餐馆\ncān guǎn\nrestaurant`
+},
+
+{
+type:4,
+front:'你有____吗？',
+back:`Do you have a ____?\n\n你有____吗？\nnǐ yǒu ____ ma?\n\nOptions:\n\n1. 驾照\njià zhào\ndriver's licence\n\n2. 简历\njiǎn lì\nCV / resume\n\n3. 时间\nshí jiān\ntime\n\n4. 工作\ngōng zuò\nwork / job\n\n5. 听力\ntīng lì\nlistening ability`
+},
+
+{
+type:4,
+front:'这个公司的员工都是____。',
+back:`All the employees at this company are ____.\n\n这个公司的员工都是____。\nzhè ge gōng sī de yuán gōng dōu shì ____.\n\nOptions:\n\n1. 年轻人\nnián qīng rén\nyoung people\n\n2. 老人\nlǎo rén\nelderly people\n\n3. 外国人\nwài guó rén\nforeigners\n\n4. 留学生\nliú xué shēng\ninternational students\n\n5. 经理\njīng lǐ\nmanagers`
+},
+
+{
+type:4,
+front:'学外语需要练习____。',
+back:`Learning a foreign language requires practising ____.\n\n学外语需要练习____。\nxué wài yǔ xū yào liàn xí ____.\n\nOptions:\n\n1. 听力\ntīng lì\nlistening\n\n2. 口语\nkǒu yǔ\nspeaking\n\n3. 阅读\nyuè dú\nreading\n\n4. 写作\nxiě zuò\nwriting\n\n5. 翻译\nfān yì\ntranslation`
+},
+
+{
+type:4,
+front:'你在哪个____工作？',
+back:`Which ____ do you work in?\n\n你在哪个____工作？\nnǐ zài nǎ ge ____ gōng zuò?\n\nOptions:\n\n1. 部门\nbù mén\ndepartment\n\n2. 公司\ngōng sī\ncompany\n\n3. 办公室\nbàn gōng shì\noffice\n\n4. 学校\nxué xiào\nschool\n\n5. 城市\nchéng shì\ncity`
+},
+
+{
+type:4,
+front:'这是一家____公司。',
+back:`This is a ____ company.\n\n这是一家____公司。\nzhè shì yī jiā ____ gōng sī.\n\nOptions:\n\n1. 科技\nkē jì\ntechnology\n\n2. 保健\nbǎo jiàn\nhealthcare\n\n3. 旅游\nlǚ yóu\ntravel\n\n4. 教育\njiào yù\neducation\n\n5. 法律\nfǎ lǜ\nlaw`
+},
+
+{
+type:4,
+front:'我每天在____上班。',
+back:`Every day I go to work at the ____.\n\n我每天在____上班。\nwǒ měi tiān zài ____ shàng bān.\n\nOptions:\n\n1. 办公室\nbàn gōng shì\noffice\n\n2. 公司\ngōng sī\ncompany\n\n3. 学校\nxué xiào\nschool\n\n4. 医院\nyī yuàn\nhospital\n\n5. 餐馆\ncān guǎn\nrestaurant`
+},
+
+]);
